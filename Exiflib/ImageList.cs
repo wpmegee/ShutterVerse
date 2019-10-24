dd7d7ab88c7f54e80e14a74cadfb4da6401edf68
Will Megee <will.megee@gmail.com>
2019-10-24 17:43:24 -0500

    correct sorting

---
+++ Exiflib/ImageList.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExifLib
+{
+    public class ImageList
+    {
+        private readonly string _path;
+        private string[] _fileTypes;
+        public List<ImageWithExif> list;
+
+        public IOrderedEnumerable<string> FocalLengths => FocalLengthsDouble.Select(d => d.ToString("0.00")).OrderBy(a=>1);
+
+        public IOrderedEnumerable<double> FocalLengthsDouble => list.GroupBy(l => l.FocalLengthDouble)
+                                  .Select(g => g.Key).OrderBy(g => g);
+
+        public IEnumerable<int> FocalLengthCounts => list.GroupBy(l => l.FocalLengthDouble)
+                                  .Select(g => g.Select(l => l.FocalLengthDouble).Count());
+
+
+        public ImageList(string path)
+        {
+            _path = path;
+            list = new List<ImageWithExif>();
+            _fileTypes = new string[] { "*.jpg", "*.cr2" };
+        }
+
+        public void Load()
+
+        {
+            foreach (var type in _fileTypes)
+            {
+                foreach (var file in Directory.EnumerateFiles(_path, type, SearchOption.AllDirectories))
+                {
+                    try
+                    {
+                        list.Add(new ImageWithExif(file));
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }         
+        }
+    }
+}
